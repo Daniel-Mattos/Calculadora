37a9d6c31b2f3365d6c0e156c574b963ac487968
daniel.mattos@negocieonline.com.br <daniel.mattos@negocieonline.com.br>
2025-07-31 15:38:36 -0300

    criando uma calculadora com react

---
+++ src/components/Button/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const ButtonContainer = styled.button`
+  padding: 0.5rem;
+  border: 1px solid #cdcdcd;
+  background-color: #00aaf0;
+  color: #fff;
+  font-size: 24px;
+  font-weight: 700;
+  flex:1;
+`
